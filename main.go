3b8580b4ec0afcda8f5c0876aa6a8a0869b0422b
Rafey Ahmad <rafeyahmad@protonmail.com>
2024-01-09 22:44:41 +0530

    Add grade calculation logic to calculateGrade function

--- main.go
+++ main.go
@@ -25,7 +25,26 @@ func parseCSV(filePath string) []student {
 }
 
 func calculateGrade(students []student) []studentStat {
-	return nil
+	studentStats := make([]studentStat, 0, len(students))
+	for _, student := range students {
+		score := float32((student.test1Score + student.test2Score + student.test3Score + student.test4Score) / 4)
+		var grade string
+		switch {
+		case score < 35:
+			grade = "F"
+		case score >= 35 && score < 50:
+			grade = "D"
+		case score >= 50 && score < 70:
+			grade = "B"
+		case score >= 70:
+			grade = "A"
+		}
+
+		stat := studentStat{student, score, Grade(grade)}
+		studentStats = append(studentStats, stat)
+	}
+
+	return studentStats
 }
 
 func findOverallTopper(gradedStudents []studentStat) studentStat {
